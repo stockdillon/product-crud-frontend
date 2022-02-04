d37fb49aebd14710ee5662f6099c3987140d8f85
Dillon <dillon.w.stock@gmail.com>
2022-02-04 04:04:20 -0700

    render edit component on separate page

--- src/app/products/product-page-routing.module.ts
+++ src/app/products/product-page-routing.module.ts
@@ -4,14 +4,15 @@ import { ProductPageComponent } from './product-page.component';
 
 const routes: Routes = [
     // { outlet:'dialog', path: 'create', loadChildren: () => import('./product-create/product-create.module').then(m => m.ProductCreateModule) },
-    { path: '', pathMatch: 'full', redirectTo: 'list'},
-    { path: '', component: ProductPageComponent, children: [
-        { path: 'list', loadChildren: () => import('./product-list/product-list.module').then(m => m.ProductListModule) },
-        { path: 'edit/:name', loadChildren: () => import('./product-details/product-details.module').then(m => m.ProductDetailsModule) },
-        { path: 'create', loadChildren: () => import('./product-create/product-create.module').then(m => m.ProductCreateModule) },
-    ]},
+    { path: 'edit/:name', loadChildren: () => import('./product-details/product-details.module').then(m => m.ProductDetailsModule), pathMatch: 'full' },
+    { path: '', component: ProductPageComponent, pathMatch: 'full' },
+    // { path: '', pathMatch: 'full', redirectTo: 'list'},
+    // { path: '', component: ProductPageComponent, children: [
+    //     { path: 'list', loadChildren: () => import('./product-list/product-list.module').then(m => m.ProductListModule) },
+    //     // { path: 'create', loadChildren: () => import('./product-create/product-create.module').then(m => m.ProductCreateModule) },
+    // ]},
     // { outlet:'dialog', path: 'create', loadChildren: () => import('./product-create/product-create.module').then(m => m.ProductCreateModule) },
-    // { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
